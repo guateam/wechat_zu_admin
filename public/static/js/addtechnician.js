761157e5d48d7cf6ceb6e7637c00614ba69a0ef5
Unknown <zhangyu199946@gmail.com>
2018-08-24 20:38:12 +0800

    修复bug

--- public/static/js/addtechnician.js
+++ public/static/js/addtechnician.js
@@ -3,21 +3,20 @@ $(document).ready(function() {
     var gender = "";
     var phone = "";
     var job_number = "";
-    $("#submit-btn").on("click",()=>{
-        if($("#username")!='' &&  $("input:radio[name='sex'][checked]")!='' && $("#mobile")!='' && $("#job_number") !='')
-        {
+    $("#submit-btn").on("click", () => {
+        if ($("#username") != '' && $("input:radio[name='sex'][checked]") != '' && $("#mobile") != '' && $("#job_number") != '') {
             name = $("#username").val();
             gender = $("input:radio[name = 'sex']:checked").val();
             phone = $("#mobile").val();
             job_number = $("#job_number").val();
-            
-            $.post("/wechat_zu/public/index.php/api/Technician/add_technician",{
-                name:name,
-                gender:gender,
-                mobile:phone,
-                job_number:job_number
-            }).done(function(result){
-                if(result.status == 1){
+
+            $.post("/wechat_zu_admin/public/index.php/api/Technician/add_technician", {
+                name: name,
+                gender: gender,
+                mobile: phone,
+                job_number: job_number
+            }).done(function(result) {
+                if (result.status == 1) {
                     parent.location.reload()
                     var index = parent.layer.getFrameIndex(window.name);
                     parent.layer.close(index);

181accaf400f2cc6430a3cc5ffbe5157522a3c79
Unknown <zzwyxl@163.com>
2018-08-24 09:21:20 +0800

    不知道为什么突然消失了2个文件...
    补上了

---
+++ application/admin/controller/Index.php
@@ -0,0 +1,21 @@
+<?php
+namespace app\admin\controller;
+use think\Controller;
+use \app\api\model\admin as UserModel;
+/**
+ * 工作情况页面
+ */
+class Index extends Controller{
+    /**
+     * 全局工作情况页面
+     */
+    public function index(){
+        //if(isset($_COOKIE['userid'])){
+        //    $user=new \app\api\controller\admin();
+        //    if($user->checkuser($_COOKIE['userid'])){
+                return $this->fetch('index');
+        //    }
+        //}
+        //return $this->error('请先登录','index/index/index');
+    }
+}

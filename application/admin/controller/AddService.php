d5258e545bbb6d97ae36a494a9937b2c4a80de4e
-LAN- <laipz8200@icloud.com>
2018-08-27 17:09:26 +0800

    merge

--- application/admin/controller/AddService.php
+++ application/admin/controller/AddService.php
@@ -3,7 +3,9 @@ namespace app\admin\controller;
 use think\Controller;
 use \app\api\model\ServiceType as UserModel;
 
+class Addservice extends Controller{
 
+    public function Addservice(){
         $ctrl = new \app\api\controller\Servicetype();
         return $this->fetch('addservice');
     }

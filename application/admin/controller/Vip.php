3bf85464591f54c9b3ba269d73423145698d0bff
Unknown <zzwyxl@163.com>
2018-08-28 12:43:20 +0800

    修复部分问题

--- application/admin/controller/Vip.php
+++ application/admin/controller/Vip.php
@@ -12,4 +12,9 @@ class Vip extends Controller{
         return $this->fetch('vip');
 
     }
+
+    public function edit($vip){
+        $this->assign("vip",$vip);
+        return $this->fetch("edit_vip");
+    }
 }

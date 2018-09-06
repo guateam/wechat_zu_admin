871b46be76ba71273532ec60ddaedbfbd5512105
Unknown <zzwyxl@163.com>
2018-09-06 10:09:29 +0800

    22，23解决

--- application/admin/controller/Comment.php
+++ application/admin/controller/Comment.php
@@ -7,14 +7,20 @@ class Comment extends Controller{
     public function index(){
         $att = \app\api\model\Rate::all();
         $svs = new \app\api\controller\Servicetype();
+        $svod = new \app\api\controller\Serviceorder();
         foreach($att as $rt){
             $name = $svs->getservicename($rt->service_id);
+
             if($name){
+                $so = $svod->get_order($rt->order_id);
                 $rt->service_id = $name;
+                $rt->job_number = $so[0]->job_number;
             }
         }
+        $tech = \app\api\model\Technician::all();
         $this->assign("comment",$att);
         $this->assign("count",count($att));
+        $this->assign("tech",$tech);
         return $this->fetch('comment');
     }
 }

9e60cca710f7240cb37c8ecaf2df09ed352e909a
Unknown <zhangyu199946@gmail.com>
2018-10-10 13:11:35 +0800

    打赏记录

---
+++ application/admin/controller/Tip.php
@@ -0,0 +1,14 @@
+<?php
+namespace app\admin\controller;
+use think\Controller;
+use \app\api\model\Technician as UserModel;
+
+class Tip extends Controller{
+    public function index(){
+        $tip=new \app\api\controller\Tip();
+        $data=$tip->gettiplist();
+        $this->assign('count',count($data));
+        $this->assign('list',$data);
+        return $this->fetch('tip');
+    }
+}
---
+++ application/api/model/Tip.php
@@ -0,0 +1,8 @@
+<?php
+namespace app\api\model;
+use think\Model;
+
+class Tip extends Model{
+    //普通商品表
+    protected $table = 'tip';
+}

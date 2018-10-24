28d27c7c8a6e9ebdc8189afff3696af0b9e2186a
yun_cn <zzwyxl@163.com>
2018-10-24 15:07:06 +0800

    管理员指定技师头像

--- application/api/controller/Technician.php
+++ application/api/controller/Technician.php
@@ -319,4 +319,75 @@
             }
             return $data;
         }
+        public function upload(){
+            $dir = $_SERVER['DOCUMENT_ROOT']."/photo/";
+            $save_dir = "/photo/";
+            $job_number=$_POST['job_number'];
+            $allowedExts = array("gif", "jpeg", "jpg", "png","PNG");
+            $temp = explode(".", $_FILES["file"]["name"]);
+            $extension = end($temp);        // 获取文件后缀名
+
+            $dict=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u',
+                    'v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P',
+                    'Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9','0'];
+            $rnd_str = "";
+            for($i = 0;$i<7;$i++)
+            {
+                $idx = rand(0,count($dict)-1);
+                $rnd_str.=$dict[$idx];    
+            }
+
+            if ((
+                ($_FILES["file"]["type"] == "image/jpeg") 
+                ||  ($_FILES["file"]["type"] == "image/jpg")
+                || ($_FILES["file"]["type"] == "image/x-png")
+                || ($_FILES["file"]["type"] == "image/png"))   
+            )
+            {
+                if ($_FILES["file"]["error"] > 0)
+                {
+                    return json_encode(["state"=>$_FILES["file"]["error"]]);
+                }
+                else
+                {
+                    // 判断当期目录下的 upload 目录是否存在该文件
+                    // 如果没有 upload 目录，你需要创建它，upload 目录权限为 777
+                    if (file_exists( $dir . $_FILES["file"]["name"]))
+                    {
+                        return json_encode(["state"=>'文件已经存在']);
+                    }
+                    else
+                    {
+                        $tm = date("ymdhis",time());
+                        $sv = $save_dir.$rnd_str.$tm.$_FILES["file"]["name"];
+                        $tm=$dir.$rnd_str.$tm.$_FILES["file"]["name"];
+                        $tc = UserModel::get(["job_number"=>$job_number]);
+                        // 如果 upload 目录不存在该文件则将文件上传到 upload 目录下
+                        if($tc)
+                        {
+                            if($tc->photo){
+                                unlink($_SERVER['DOCUMENT_ROOT'].$tc->photo);
+                            }
+                            move_uploaded_file($_FILES["file"]["tmp_name"],$tm );
+                            $tf = new \app\api\model\Technicianphoto();
+                            $tf->job_number = $job_number;
+                            $tf->img = $sv;
+                            $tf->save();
+                            $tc->photo = $sv;
+                            $tc->save();
+                           return json_encode(["state"=>1,'url'=>$sv]);
+                        }
+                        else
+                        {
+                           return json_encode(["state"=>0]);
+                        }
+                    }
+                }
+
+            }
+            else
+            {
+                return json_encode(["state"=>"格式错误:".$_FILES["file"]["type"]]);
+            }
+        }
     }

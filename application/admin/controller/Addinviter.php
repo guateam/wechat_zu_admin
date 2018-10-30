134eca17261c369701b0004c06a801d086827312
yun_cn <zzwyxl@163.com>
2018-10-30 10:30:16 +0800

    指定介绍人会有默认选中

--- application/admin/controller/Addinviter.php
+++ application/admin/controller/Addinviter.php
@@ -5,12 +5,13 @@ use \app\api\model\Technician as UserModel;
 
 class Addinviter extends Controller{
 
-    public function Addinviter($jbnb){
+    public function Addinviter($jbnb,$inv_jbnb = null){
         $ctrl =new \app\api\controller\Technician();
         $technicians = $ctrl->get_all_technician();
         $count = $ctrl->count_all();
         $this->assign("technicians",$technicians);
         $this->assign("self_job_number",$jbnb);
+        $this->assign("inv_job_number",$inv_jbnb);
         return $this->fetch("Addinviter/addinviter");
     }
 }

6fd01db503708a319357af669308b0d21f87f6f9
yun_cn <zzwyxl@163.com>
2019-02-17 21:19:53 +0800

    打赏，显示用户名和openid

--- application/api/controller/Tip.php
+++ application/api/controller/Tip.php
@@ -1,14 +1,22 @@
 <?php 
 namespace app\api\controller;
 use think\Controller;
+use think\Db;
 
 class Tip extends Controller{
     public function gettiplist(){
-        $tip=\app\api\model\Tip::all();
+        $tip=Db::query("select * from tip");
         for($i=0;$i<count($tip);$i++){
-            $tip[$i]->date = date("Y-m-d H:i:s", $tip[$i]->date);
+            $openid = $tip[$i]['user_id'];
+            $name = Db::query("select name from customer where openid = '$openid'");
+            $tip[$i]['date'] = date("Y-m-d H:i:s", $tip[$i]['date']);
+            if($name){
+                $tip[$i]['username'] = $name[0]['name'];
+            }else{
+                $tip[$i]['username'] = "用户不存在";
+            }
         }
         return $tip;
     }
 
-}
+} 

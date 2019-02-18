f92ccf16f34eb263455ab5cbf9afa2b95bc0ce53
yun_cn <zzwyxl@163.com>
2019-02-18 13:16:46 +0800

    客户充卡返现规则在店铺管理-活动设置里可以设定

--- application/api/controller/Promotion.php
+++ application/api/controller/Promotion.php
@@ -9,12 +9,12 @@ class Promotion extends Controller{
         return json(['status'=>1]);
     }
     public function get($id){
-        $data = Db::query("select ID,content,need,back,FROM_UNIXTIME(A.start,'%Y-%m-%d %H:%i:%s') as _start,FROM_UNIXTIME(A.end,'%Y-%m-%d %H:%i:%s') as _end from promotion A where ID='$id'");
+        $data = Db::query("select ID,content,need,back,FROM_UNIXTIME(A.start,'%Y-%m-%d') as _start,FROM_UNIXTIME(A.end,'%Y-%m-%d') as _end from promotion A where ID='$id'");
         //$data = \app\api\model\Promotion::get(["ID"=>$id]);
         return $data;
     }
     public function get_all(){
-        $data = Db::query("select ID,content,need,back,FROM_UNIXTIME(A.start,'%Y-%m-%d %H:%i:%s') as _start,FROM_UNIXTIME(A.end,'%Y-%m-%d %H:%i:%s') as _end from promotion A");
+        $data = Db::query("select ID,content,need,back,FROM_UNIXTIME(A.start,'%Y-%m-%d') as _start,FROM_UNIXTIME(A.end,'%Y-%m-%d') as _end from promotion A");
         return $data;
     }
     public function add($content,$need,$back,$start,$end){
@@ -29,6 +29,14 @@ class Promotion extends Controller{
     }
     public function update($id,$content,$need,$back,$start,$end){
         $data = \app\api\model\Promotion::get(["ID"=>$id]);
+        //将字符串日期转换成时间戳
+        if(gettype($start) == "string"){
+            $start = strtotime($start);
+
+        }
+        if(gettype($end) == "string"){
+            $end = strtotime($end);
+        }
         $data->content = $content;
         $data->need = $need;
         $data->back = $back;

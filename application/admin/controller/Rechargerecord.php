4f1e04fdc5f9a333f85adfc6d4566faade5bdd29
yun_cn <zzwyxl@163.com>
2019-02-19 14:54:30 +0800

    会员列表，账单按钮和新页面

--- application/admin/controller/Rechargerecord.php
+++ application/admin/controller/Rechargerecord.php
@@ -1,7 +1,7 @@
 <?php
 namespace app\admin\controller;
 use think\Controller;
-
+use think\Db;
 class Rechargerecord extends Controller{
 
     public function index($edit,$username = ""){
@@ -12,5 +12,29 @@ class Rechargerecord extends Controller{
         $this->assign("count",count($record));
         $this->assign("edit",$edit);
         return $this->fetch('Rechargerecord/recharge');
+    }
+
+    public function bill($edit,$openid = ""){
+        $recharge_record = Db::query("select A.record_id as ID,A.user_id as openid,A.charge/100 as money,A.job_number as job_number,A.payment_method as payment_method,A.generated_time as generated_time,A.note as note,B.`name` as username from recharge_record A,customer B where A.user_id='$openid' and B.openid = A.user_id");
+        $pay_record = Db::query("select A.order_id as ID,C.openid as openid,C.`name` as `username`,A.pay_amount as money,'' as job_number,A.payment_method as payment_method,A.generated_time as generated_time,A.note as note from consumed_order A,customer C where A.user_id='$openid' and C.openid = A.user_id order by A.generated_time desc");
+        
+        for($i=0;$i<count($pay_record);$i++){
+            $pay_record[$i]['note'] .= ' 服务支出';
+            $pay_record[$i]['generated_time'] = date("Y-m-d H:i:s",$pay_record[$i]['generated_time']);
+            $pay_record[$i]['money'] = intval($pay_record[$i]['money']);
+        }
+        for($i=0;$i<count($recharge_record);$i++){
+            if( $recharge_record[$i]['note'] == ""){
+                $recharge_record[$i]['note'] = "充值";
+            }
+            $recharge_record[$i]['generated_time'] = date("Y-m-d H:i:s",$recharge_record[$i]['generated_time']);
+            $recharge_record[$i]['money'] = intval($recharge_record[$i]['money']);
+        }
+        $total_record = array_merge($recharge_record,$pay_record);
+
+        $this->assign('record',$total_record);
+        $this->assign('count',count($total_record));
+        $this->assign('edit',$edit);
+        return $this->fetch('Rechargerecord/bill');
     }
 }
--- application/api/controller/Admin.php
+++ application/api/controller/Admin.php
@@ -131,4 +131,57 @@
             Db::query("delete from admin where `ID`='$id'");
             return json(['status'=>1]);
         }
+
+        public function serve_side($table,$primary_key){
+            // DB table to use
+            $table = 'datatables_demo';
+ 
+            // Table's primary key
+            $primaryKey = 'id';
+ 
+            // Array of database columns which should be read and sent back to DataTables.
+            // The `db` parameter represents the column name in the database, while the `dt`
+            // parameter represents the DataTables column identifier. In this case simple
+            // indexes
+            $columns = array(
+                array( 'db' => 'first_name', 'dt' => 0 ),
+                array( 'db' => 'last_name',  'dt' => 1 ),
+                array( 'db' => 'position',   'dt' => 2 ),
+                array( 'db' => 'office',     'dt' => 3 ),
+                array(
+                    'db'        => 'start_date',
+                    'dt'        => 4,
+                    'formatter' => function( $d, $row ) {
+                        return date( 'jS M y', strtotime($d));
+                    }
+                ),
+                array(
+                    'db'        => 'salary',
+                    'dt'        => 5,
+                    'formatter' => function( $d, $row ) {
+                        return '$'.number_format($d);
+                    }
+                )
+            );
+ 
+            // SQL server connection information
+            $sql_details = array(
+                'user' => '',
+                'pass' => '',
+                'db'   => '',
+                'host' => ''
+            );
+ 
+ 
+            /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+             * If you just want to use the basic configuration for DataTables with PHP
+             * server-side, there is no need to edit below this line.
+             */
+ 
+            require( 'ssp.class.php' );
+ 
+            echo json_encode(
+                SSP::simple( $_GET, $sql_details, $table, $primaryKey, $columns )
+            );
+        }
     }

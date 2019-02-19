d6b15c1f87769de4854078409c8ee7918eb87fe6
yun_cn <zzwyxl@163.com>
2019-02-19 14:59:52 +0800

    更新

--- application/api/controller/Customer.php
+++ application/api/controller/Customer.php
@@ -26,10 +26,10 @@
             $records = Db::query("select sum(charge)/100 as charge from recharge_record where user_id='$openid' group by user_id");
             $charge =0;
             if(count($records)>0)
-                $charge = (int)$records[0]['charge'];
-            $pay = Db::query("select sum(pay_amount)/100 as pay from consumed_order where user_id='$openid' and (state=4 or state=5) and payment_method=3");
+                $charge = intval($records[0]['charge']);
+            $pay = Db::query("select sum(pay_amount)/100 as pay from consumed_order where user_id='$openid' and (state!=3) and payment_method=3");
             if(count($pay)>0)
-                $pay = (int)$pay[0]['pay'];
+                $pay = intval($pay[0]['pay']);
             $money = $charge - $pay;
             return $money;
         }

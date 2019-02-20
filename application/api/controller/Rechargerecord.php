97478c5df4bec62505b30630bbaad588c0b81f15
wilson <thkdragon@cjlu.edu.cn>
2019-02-20 20:17:05 +0800

    再次提交

--- application/api/controller/Rechargerecord.php
+++ application/api/controller/Rechargerecord.php
@@ -1,96 +1,53 @@
-<?php 
-    namespace app\api\controller;
-    use think\Controller;
-    use \app\api\model\Rechargerecord as UserModel;
-    use think\Db;
-    class Rechargerecord extends Controller{
+<?php
+namespace app\admin\controller;
+use think\Controller;
+use think\Db;
+class Rechargerecord extends Controller{
 
-        public function get_all(){
-            $all = Db::query("select * from recharge_record");//UserModel::all();
-            for($i=0;$i<count($all);$i++){
-                $openid = $all[$i]['user_id'];
-                $all[$i]['generated_time'] = date('Y-m-d H:i:s',$all[$i]['generated_time']);
-                $name = Db::query("select * from customer where openid = '$openid'");
-                if($name){
-                    $all[$i]['username'] =$name[0]['name'];
-                }else{
-                    $all[$i]['username'] ="用户不存在";
-                }
-
-            }
-            return $all;
-        }
-        public function get_all_by_user(){
-            $all_cus = Db::query("select * from customer group by openid");
-            $records = [];
-            foreach($all_cus as $cus){
-                //没有openid的用户属于异常数据，不进行处理
-                $cus_openid = $cus['openid'];
-                if($cus_openid == "")continue;
-
-                $record = Db::query("select sum(A.charge)/100 as charge from recharge_record A,customer B where A.user_id='$cus_openid' and B.openid=A.user_id");
-                if($record){
-                    if(is_null($record[0]['charge']) )$record[0]['charge'] = 0;
-                    $record = array_merge($record[0],['spoke_name'=>$cus['spoke_name'],'phone_number'=>$cus['phone_number'],'name'=>$cus['name'],'user_id'=>$cus['openid'],'gender'=>$cus['gender'],'level'=>$cus['level'],'registration_date'=>$cus['registration_date']]);
-                    array_push($records,$record);
-                }
-            }
-            // $records = Db::query("select sum(A.charge)/100 as charge,A.user_id,B.phone_number,B.gender,B.level,B.openid,B.registration_date from recharge_record A,customer B where A.user_id=B.openid
-            // group by B.openid");
-            $cus = new \app\api\controller\Customer();
-            for($i=0;$i<count($records);$i++){
-                if($records[$i]['registration_date'] != "")
-                    $records[$i]['registration_date'] = date("Y-m-d H:i:s", $records[$i]['registration_date']);
-                else{
-                    $records[$i]['registration_date'] = "无";
-                }
-                $records[$i]['charge'] = intval($records[$i]['charge']);
-                $money = $cus->get_cash($records[$i]['user_id']);
-                $records[$i] = array_merge($records[$i],['cash'=>$money]);
-            }
-            return $records;
-        }
-
-        /**
-         * 获取某个工号的技师一段时间内的充卡额，默认本月
-         * 
-         */
-        public function get_by_jobnumber($num,$start="",$end=""){
-            if($start == "")
-                $start=strtotime(date('Y-m-01', strtotime(date("Y-m-d"))));
-            if($end == "")
-                $end = strtotime("$start +1 month -1 day");
-
-            if($end < $start){
-                $temp = $end;
-                $end = $start;
-                $start = $temp;
-            }
-            $record = Db::query("select sum(A.charge) as charge from recharge_record A where `job_number`='$num'");
-            if($record){
-                $money = $record[0]['charge']/100;
-                return json(['status'=>1,'charge'=>$money]);
-            }
-            return json(['status'=>0]);
-        }
-        /**
-         * 获取某个客户的充卡数额
-         * 
-         */
-        public function get_by_user($num){
-            $record = UserModel::all(['user_id'=>$num]);
-            if($record){
-                $money = 0;
-                foreach($record as $rcd){
-                    $time = $rcd->generated_time;
-                    $time = substr($time,0,10);
-                    if($time>=$start && $time<=$end)
-                        $money+=$rcd->charge;
-                }
-                $money/=100;
-                return json(['status'=>1,'charge'=>$money]);
-            }
-            return json(['status'=>0]);
-        }
-        
+    public function index($edit,$username = ""){
+        $ctrl = new \app\api\controller\Rechargerecord();
+        $record =$ctrl->get_all();
+        $this->assign("username",$username);
+        $this->assign("record",$record);
+        $this->assign("count",count($record));
+        $this->assign("edit",$edit);
+        return $this->fetch('Rechargerecord/recharge');
     }
+
+    public function bill($edit,$openid = ""){
+        $recharge_record = Db::query("select A.record_id as ID,A.user_id as openid,A.charge/100 as money,A.job_number as job_number,A.payment_method as payment_method,A.generated_time as generated_time,A.note as note,B.`name` as username from recharge_record A,customer B where A.user_id='$openid' and B.openid = A.user_id");
+        $pay_record = Db::query("select A.order_id as ID,C.openid as openid,C.`name` as `username`,A.pay_amount/100 as money,'' as job_number,A.payment_method as payment_method,A.generated_time as generated_time,A.note as note from consumed_order A,customer C where A.user_id='$openid' and C.openid = A.user_id order by A.generated_time desc");
+        $tip = Db::query("select A.ID as ID, A.user_id as openid,A.salary/100 as money,'微信' as payment_method, '打赏技师' as note,technician_id as job_number,date as generated_time from tip A ");
+
+        for($i=0;$i<count($pay_record);$i++){
+            $order_id = $pay_record[$i]['ID'];
+            $job_number = Db::query("select GROUP_CONCAT(job_number) as job_number from service_order where order_id = '$order_id'");
+            $pay_record[$i]['job_number'] = $job_number[0]['job_number'];
+            $pay_record[$i]['note'] .= ' 消费';
+            $pay_record[$i]['generated_time'] = date("Y-m-d H:i:s",$pay_record[$i]['generated_time']);
+            $pay_record[$i]['money'] = intval($pay_record[$i]['money']);
+        }
+        for($i=0;$i<count($recharge_record);$i++){
+            if( $recharge_record[$i]['note'] == ""){
+                $recharge_record[$i]['note'] = "充值";
+            }
+            $recharge_record[$i]['generated_time'] = date("Y-m-d H:i:s",$recharge_record[$i]['generated_time']);
+            $recharge_record[$i]['money'] = intval($recharge_record[$i]['money']);
+        }
+        for($i=0;$i<count($tip);$i++){
+            $openid = $tip[$i]['openid'];
+            $name = Db::query("select name from customer where openid='$openid'");
+            if($name){
+                $tip[$i]['username'] = $name[0]['name'];
+            }else{
+                $tip[$i]['username'] = '顾客不存在';
+            }
+        }
+        $total_record = array_merge($recharge_record,$pay_record,$tip);
+
+        $this->assign('record',$total_record);
+        $this->assign('count',count($total_record));
+        $this->assign('edit',$edit);
+        return $this->fetch('Rechargerecord/bill');
+    }
+}

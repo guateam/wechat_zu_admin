abe077149516ca6f3e7f708d073c1f71904024aa
wilson <thkdragon@cjlu.edu.cn>
2019-03-03 18:42:24 +0800

    接着上一波修改

--- application/admin/controller/Attendance.php
+++ application/admin/controller/Attendance.php
@@ -4,24 +4,17 @@ use think\Controller;
 use \app\api\model\ServiceType as UserModel;
 
 class Attendance extends Controller{
-    // public function index()
-	// {
-        // $att = \app\api\model\Attendance::all();		
-        // $this->assign("attendance",$att);
-        // $this->assign("count",count($att));
-        // return $this->fetch('Attendance/attendance');
-    // }
-	
+    
 	public function index($edit,$first_day=null,$last_day=null)
     {
         date_default_timezone_set('PRC');
 
         $ctrl = new \app\api\controller\Attendance();
 
-        //本月第一天
+      
         if(is_null($first_day))
         {
-            $first_day = strtotime(date("Y-m-01"));
+            $first_day = strtotime(date("Y-m-d")." 00:00:00");	  //当天
             $this->assign("begin",date("Y-m-01"));
         }
         else
@@ -32,8 +25,8 @@ class Attendance extends Controller{
         //本月最后一天
 
         if(is_null($last_day))
-        {			
-			$last_day = strtotime(date("Y-m-t")) + 24 * 3600 - 1;//时间戳要改下
+        {
+			$last_day = strtotime(date("Y-m-d")." 23:59:59");	//当天		
             $this->assign("end",date("Y-m-t"));
         }
         else
@@ -42,22 +35,6 @@ class Attendance extends Controller{
             $last_day = strtotime($last_day) + 24 * 3600 - 1;//时间戳要改下
         }
 
-        //$att = $ctrl->get_all_attendance($first_day,$last_day);
-
-		/*
-        $checkin = $ctrl->get_all_checkin($first_day,$last_day);
-        $checkout = $ctrl->get_all_checkout($first_day,$last_day);
-        $checkleave = $ctrl->get_all_checkleave($first_day,$last_day);
-        
-		$att = \app\api\model\Attendance::all();	
-      
-        $this->assign("attendance",$att);
-		
-		$this->assign("checkin",$checkin);
-		$this->assign("checkout",$checkout);
-		$this->assign("checkleave",$checkleave);
-		*/
-		
 		$att = $ctrl->get_all_attendance($first_day,$last_day);
 		
 		$this->assign("attendance",$att);

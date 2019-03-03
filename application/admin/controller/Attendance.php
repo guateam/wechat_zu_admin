2664480186a78021c079e78226866328791c4c31
wilson <thkdragon@cjlu.edu.cn>
2019-03-03 17:34:17 +0800

    初步完成了考勤记录页面

--- application/admin/controller/Attendance.php
+++ application/admin/controller/Attendance.php
@@ -4,10 +4,65 @@ use think\Controller;
 use \app\api\model\ServiceType as UserModel;
 
 class Attendance extends Controller{
-    public function index(){
-        $att = \app\api\model\Attendance::all();
+    // public function index()
+	// {
+        // $att = \app\api\model\Attendance::all();		
+        // $this->assign("attendance",$att);
+        // $this->assign("count",count($att));
+        // return $this->fetch('Attendance/attendance');
+    // }
+	
+	public function index($edit,$first_day=null,$last_day=null)
+    {
+        date_default_timezone_set('PRC');
+
+        $ctrl = new \app\api\controller\Attendance();
+
+        //本月第一天
+        if(is_null($first_day))
+        {
+            $first_day = strtotime(date("Y-m-01"));
+            $this->assign("begin",date("Y-m-01"));
+        }
+        else
+        {
+            $this->assign("begin",$first_day);
+            $first_day = strtotime($first_day);
+        }
+        //本月最后一天
+
+        if(is_null($last_day))
+        {			
+			$last_day = strtotime(date("Y-m-t")) + 24 * 3600 - 1;//时间戳要改下
+            $this->assign("end",date("Y-m-t"));
+        }
+        else
+        {
+            $this->assign("end",$last_day);//日期不会错
+            $last_day = strtotime($last_day) + 24 * 3600 - 1;//时间戳要改下
+        }
+
+        //$att = $ctrl->get_all_attendance($first_day,$last_day);
+
+		/*
+        $checkin = $ctrl->get_all_checkin($first_day,$last_day);
+        $checkout = $ctrl->get_all_checkout($first_day,$last_day);
+        $checkleave = $ctrl->get_all_checkleave($first_day,$last_day);
+        
+		$att = \app\api\model\Attendance::all();	
+      
         $this->assign("attendance",$att);
+		
+		$this->assign("checkin",$checkin);
+		$this->assign("checkout",$checkout);
+		$this->assign("checkleave",$checkleave);
+		*/
+		
+		$att = $ctrl->get_all_attendance($first_day,$last_day);
+		
+		$this->assign("attendance",$att);
         $this->assign("count",count($att));
+		$this->assign("edit",$edit);
         return $this->fetch('Attendance/attendance');
     }
 }
--- application/api/controller/Attendance.php
+++ application/api/controller/Attendance.php
@@ -1,8 +1,67 @@
 <?php 
     namespace app\api\controller;
     use think\Controller;
-    use \app\api\model\Attendance as UserModel;
-    class Attendance extends Controller{
+	use \app\api\model\Attendance as UserModel;
+	use think\Db;
 
+	class Attendance extends Controller
+	{
+		public function get_all_attendance($begin, $end)
+		{
+			$data = [];
+			
+			$data = [];
+			$technicians = \app\api\model\Technician::all();
+			foreach ($technicians as $tech) 
+			{
+				array_push($data, self::get_attendance($tech->job_number, $begin, $end));
+			}
+			
+			return $data;			
+		}
+		
+		public static function get_attendance($job_number, $begin, $end)
+		{
+			//考勤类型  (0-签到，1-签退  2--异常）
+			$check_in_time = "";
+			$tech_att_checkin = Db::query("select * from attendance where job_number = '$job_number' and check_time >= '$begin' and check_time <= '$end' and sign_type = 0" );
+			if ($tech_att_checkin)
+			{
+				$check_in_time = $tech_att_checkin[0]['check_time'];
+				$check_in_time = date('Y-m-d H:i:s', $check_in_time);
+			}
+			
+			$check_out_time = "";
+			$tech_att_checkout = Db::query("select * from attendance where job_number = '$job_number' and check_time >= '$begin' and check_time <= '$end' and sign_type = 1" );
+			if ($tech_att_checkout)
+			{
+				$check_out_time = $tech_att_checkout[0]['check_time'];				
+				$check_out_time = date('Y-m-d H:i:s', $check_out_time);
+				
+			}
+			
+			$check_leave_time = "";
+			$tech_att_checkleave = Db::query("select * from attendance where job_number = '$job_number' and check_time >= '$begin' and check_time <= '$end' and sign_type = 2" );
+			if ($tech_att_checkleave)
+			{
+				foreach ($tech_att_checkleave as $leave)
+				{					
+					$leave_time = date('Y-m-d H:i:s', $leave['check_time']);
+					$check_leave_time = $check_leave_time.$leave_time."  ,  ";
+				}				
+			}
+			
+			return [				
+				'job_number' => $job_number,
+				'check_in_time' => $check_in_time,
+				'check_out_time' => $check_out_time,
+				'check_leave_time' => $check_leave_time,	
+				'sign_type'=>3,
+				'check_time'=>0,
+			];
+		}
+		
+	}
 
-    }
+	
+?>
